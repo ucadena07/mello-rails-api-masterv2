6e0788191104d0c6da1d6dddfac19db332ed7a41
Ulises Cadena <ucadena07@gmail.com>
2020-03-28 09:16:28 -0500

    modals

--- public/assets/js/user-boards.js
+++ public/assets/js/user-boards.js
@@ -1,4 +1,7 @@
 const $logoutButton = $('#logout');
+const $newBoardButton = $('#new-board');
+const $boatNameInput = $('#board-name');
+const $saveBoardButton = $('#save-board');
 
 let user;
 
@@ -16,6 +19,17 @@ function init() {
   $('.welcome h1').text('Welcome ' + user.email + '!');
 }
 
+function handleBoardCreate(event) {
+  event.preventDefault();
+
+  let boardName = $boatNameInput.val().trim();
+
+  $newBoardButton.val('');
+
+  console.log(boardName);
+
+}
+
 function handleLogout() {
   $.ajax({
     url: '/logout',
@@ -27,3 +41,5 @@ function handleLogout() {
 }
 
 $logoutButton.on('click', handleLogout);
+$newBoardButton.on('click', MicroModal.show.bind(null, 'create-board'));
+$saveBoardButton.on('click', handleBoardCreate);
