fb98746e14f1d650fc52b5780dcf8346ff90e1fd
Ulises Cadena <ucadena07@gmail.com>
2020-03-30 14:07:31 -0500

    removing contributors

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -35,7 +35,9 @@ function getBoard(id) {
     renderBoard();
   })
   .catch(function(err) {
-    location.replace('/boards');
+    if (err.statusText === 'Unauthorized') {
+      location.replace('/boards');
+    }
   });
 }
 
@@ -114,10 +116,17 @@ function renderBoard() {
   makeSortable();
   renderContributors();
 }
-
 function renderContributors() {
   let $contributorListItems = board.users.map(function(user) {
-    let $contributorListItem = $('<li>').text(user.email);
+    let $contributorListItem = $('<li>');
+    let $contributorSpan = $('<span>').text(user.email);
+    let $contributorDeleteButton = $(
+      '<button class="danger">Remove</button>')
+      .data(user)
+      .on('click', handleContributorDelete);
+
+    $contributorListItem.append($contributorSpan, $contributorDeleteButton);
+
     return $contributorListItem;
   });
 
@@ -409,6 +418,22 @@ function openContributorModal() {
   displayMessage('');
 
   MicroModal.show('contribute');
+}
+
+function handleContributorDelete(event) {
+  let { id, email } = $(event.target).data();
+
+  $.ajax({
+    url: '/api/user_boards',
+    method: 'DELETE',
+    data: {
+      user_id: id,
+      board_id: board.id
+    }
+  }).then(function() {
+    init();
+    displayMessage(`Successfully removed user: ${email}`, 'success');
+  });
 }
 
 $contributorModalSaveButton.on('click', handleContributorSave);
